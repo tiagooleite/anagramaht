9411b16f83a77a20edf22890615cc803deb05497
Heitor Meira <htrmeira@gmail.com>
2012-03-10 18:10:25 -0300

    Adicionado os metodos para pegar anagramas e palavra embaralhada em Jogo

--- src/ufcg/les/anagrama/model/Jogo.java
+++ src/ufcg/les/anagrama/model/Jogo.java
@@ -1,15 +1,24 @@
 package ufcg.les.anagrama.model;
 
+import java.util.List;
+import java.util.Random;
+
 import ufcg.les.anagrama.enummeration.Nivel;
+import ufcg.les.anagrama.persistence.dao.PalavrasDAO;
+import ufcg.les.anagrama.util.GeradorStrings;
 
 public class Jogo {
 	
 	private String nomeJogador;
 	private int pontuacao;
 	private Nivel nivel = Nivel.NORMAL;
+	private PalavrasDAO palavrasDAO;
+	private String palavraEmbaralhada;
+	private List<String> anagramas;
 	
 	public Jogo(String nomeJogador) {
 		this.nomeJogador = nomeJogador;
+		palavrasDAO = new PalavrasDAO();
 	}
 
 	public String getNomeJogador() {
@@ -36,7 +45,27 @@ public class Jogo {
 		this.nivel = nivel;
 	}
 	
+	public String getPalavraEmbaralhada() {
+		return this.palavraEmbaralhada;
+	}
 	
+	public List<String> getAnagramas() {
+		return this.anagramas;
+	}
 	
-
+	public void carregarNovoAnagrama(){
+		this.anagramas = getListaAnagramasAleatoria(palavrasDAO.getPalavrasPorNivel(getNivel()));
+		this.palavraEmbaralhada = getPalavraEmbaralhada(this.anagramas);
+	}
+	
+	private String getPalavraEmbaralhada(List<String> anagramas) {
+		return GeradorStrings.embaralhaPalavra(anagramas.get(0));
+	}
+	
+	private List<String> getListaAnagramasAleatoria(List<List<String>> palavrasPorNivel) {
+		int tamanho = palavrasPorNivel.size();
+		Random gerador = new Random();
+		int tamanhoAleatorio = gerador.nextInt(tamanho);
+		return palavrasPorNivel.get(tamanhoAleatorio);		
+	}
 }
