1df7c20d6522048b57378e934b8abd621789d865
Heitor Meira <htrmeira@gmail.com>
2012-03-12 23:27:33 -0300

    Adiconado mais alguns anagramas

--- src/ufcg/les/anagrama/persistence/dao/PalavrasDAO.java
+++ src/ufcg/les/anagrama/persistence/dao/PalavrasDAO.java
@@ -31,16 +31,100 @@ public class PalavrasDAO extends GenericDAOImpl<String> {
 	private void carregarPalavras() {
 		List<List<String>> listaFacil = new ArrayList<List<String>>();
 		List<List<String>> listaNormal = new ArrayList<List<String>>();
+		List<List<String>> listaDificil = new ArrayList<List<String>>();
 		
 		ArrayList<String> anagramasAmor = carregaAnagramaAmor();
+		ArrayList<String> anagramasRato = carregaAnagramaRato();
+		ArrayList<String> anagramasFio = carregaAnagramaFio();
+		
 		ArrayList<String> anagramasPrato = carregaAnagramaPrato();
+		ArrayList<String> anagramasMorto = carregaAnagramaMorto();
+		ArrayList<String> anagramasOlhar = carregaAnagramaOlhar();
+		ArrayList<String> anagramasBolero = carregaAnagramaBolero();
+		ArrayList<String> anagramasRancor = carregaAnagramaRancor();
+		ArrayList<String> anagramasSacar = carregaAnagramaSacar();
+		
+		ArrayList<String> anagramasGerador = carregaAnagramaGerador();
+		ArrayList<String> anagramasRasteira = carregaAnagramaRasteira();
+		ArrayList<String> anagramasEstragar = carregaAnagramaEstragar();
 		
 		listaFacil.add(anagramasAmor);
-		listaNormal.add(anagramasPrato);
+		listaFacil.add(anagramasRato);
+		listaFacil.add(anagramasFio);
 		
-		palavrasPorNivel.put(Nivel.FACIL, listaFacil);
-		palavrasPorNivel.put(Nivel.NORMAL, listaNormal);
-		palavrasPorNivel.put(Nivel.DIFICIL, listaFacil);
+		listaNormal.add(anagramasPrato);
+		listaNormal.add(anagramasMorto);
+		listaNormal.add(anagramasOlhar);
+		listaNormal.add(anagramasBolero);
+		listaNormal.add(anagramasRancor);
+		listaNormal.add(anagramasSacar);
+		
+		listaDificil.add(anagramasGerador);
+		listaDificil.add(anagramasRasteira);
+		listaDificil.add(anagramasEstragar);
+		
+		palavrasPorNivel.put(Nivel.FACIL, listaFacil); // 3 e 4 letras
+		palavrasPorNivel.put(Nivel.NORMAL, listaNormal); // 5 e 6 letras
+		palavrasPorNivel.put(Nivel.DIFICIL, listaDificil); // >= 7 letras
+	}
+	
+	private ArrayList<String> carregaAnagramaGerador() {
+		ArrayList<String> anagramasGerador = new ArrayList<String>();
+		anagramasGerador.add("gerador");
+		anagramasGerador.add("regador");
+		anagramasGerador.add("regrado");
+		return anagramasGerador;
+	}
+	
+	private ArrayList<String> carregaAnagramaRasteira() {
+		ArrayList<String> anagramasRasteira = new ArrayList<String>();
+		anagramasRasteira.add("rasteira");
+		anagramasRasteira.add("traseira");
+		return anagramasRasteira;
+	}
+	
+	private ArrayList<String> carregaAnagramaEstragar() {
+		ArrayList<String> anagramasRasteira = new ArrayList<String>();
+		anagramasRasteira.add("estragar");
+		anagramasRasteira.add("resgatar");
+		return anagramasRasteira;
+	}
+	
+	private ArrayList<String> carregaAnagramaMorto() {
+		ArrayList<String> anagramasMorto = new ArrayList<String>();
+		anagramasMorto.add("morto");
+		anagramasMorto.add("motor");
+		return anagramasMorto;
+	}
+	
+	private ArrayList<String> carregaAnagramaSacar() {
+		ArrayList<String> anagramasMorto = new ArrayList<String>();
+		anagramasMorto.add("sacar");
+		anagramasMorto.add("casar");
+		anagramasMorto.add("sacra");
+		return anagramasMorto;
+	}
+	
+	private ArrayList<String> carregaAnagramaBolero() {
+		ArrayList<String> anagramasBolero = new ArrayList<String>();
+		anagramasBolero.add("rebolo");
+		anagramasBolero.add("bolero");
+		return anagramasBolero;
+	}
+	
+	private ArrayList<String> carregaAnagramaRancor() {
+		ArrayList<String> anagramasBolero = new ArrayList<String>();
+		anagramasBolero.add("rancor");
+		anagramasBolero.add("roncar");
+		return anagramasBolero;
+	}
+	
+	private ArrayList<String> carregaAnagramaOlhar() {
+		ArrayList<String> anagramasOlhar = new ArrayList<String>();
+		anagramasOlhar.add("olhar");
+		anagramasOlhar.add("ralho");
+		anagramasOlhar.add("rolha");
+		return anagramasOlhar;
 	}
 
 	private ArrayList<String> carregaAnagramaPrato() {
@@ -65,5 +149,19 @@ public class PalavrasDAO extends GenericDAOImpl<String> {
 		return anagramasAmor;
 	}
 	
-
+	private ArrayList<String> carregaAnagramaRato() {
+		ArrayList<String> anagramasRato = new ArrayList<String>();
+		anagramasRato.add("rato");
+		anagramasRato.add("rota");
+		anagramasRato.add("toar");
+		anagramasRato.add("tora");
+		return anagramasRato;
+	}
+	
+	private ArrayList<String> carregaAnagramaFio() {
+		ArrayList<String> anagramasFio = new ArrayList<String>();
+		anagramasFio.add("fio");
+		anagramasFio.add("foi");
+		return anagramasFio;
+	}
 }
